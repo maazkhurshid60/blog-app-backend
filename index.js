a026427a7683ebabbdfd7a60f69063539994736e
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-07-01 15:21:02 +0500

    New Changes

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const myConfigs = require("./conf/conf");
-const corsConfig = require("./conf/corsConfig");
-const path = require("path");
-const express = require("express");
-const connectToDB = require("./dbConfig/connection");
-const authRouter = require("./routes/client/auth.route");
-const clientRouter = require("./routes/client/blog.route");
-const userRouter = require("./routes/client/user.route");
-const cors = require("cors");
-const { authMiddleware } = require("./middlewares/auth.middleware");
+const myConfigs = require('./conf/conf');
+const corsConfig = require('./conf/corsConfig'); 
+const path = require('path');
+const express = require('express');
+const connectToDB = require('./dbConfig/connection');
+const authRouter = require('./routes/client/auth.route')
+const clientRouter = require('./routes/client/blog.route');
+const userRouter = require('./routes/client/user.route');
+const cors = require('cors');
+const { authMiddleware } = require('./middlewares/auth.middleware')
 
-//Adding this line for Git Activity
 
 //Created an express app
 const app = express();
@@ -22,18 +21,20 @@ app.use(cors(corsConfig));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.resolve("public/")));
+app.use(express.static(path.resolve('public/')))
+
 
 //Client Side Auth Router
-app.use("/api", authRouter);
+app.use('/api', authRouter);
 
 //Client Side Blogs Router
-app.use("/api", authMiddleware, clientRouter);
+app.use('/api', authMiddleware, clientRouter);
 
 //Client Side Users Router
-app.use("/api", authMiddleware, userRouter);
+app.use('/api', authMiddleware, userRouter);
 
 //Started to listen the server
-app.listen(myConfigs.port, () =>
-  console.log(`Server is Started at PORT: ${myConfigs.port}`)
-);
+app.listen(myConfigs.port, () => console.log(`Server is Started at PORT: ${myConfigs.port}`));
+
+
+
