13aee7ca8b67cd5bc18a78539bffbbf864e52845
Noor ur Rehman <135664580+TheRehmandev@users.noreply.github.com>
2024-07-08 22:49:58 +0500

    Commiting chnages for git Activity

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const userRouter = require('./routes/client/user.route');
 const cors = require('cors');
 const { authMiddleware } = require('./middlewares/auth.middleware')
 
-
-// Adding line for git activity
-
-
+// Adding this line for git Activity
 //Created an express app
 const app = express();
 
